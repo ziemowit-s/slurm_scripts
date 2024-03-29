b67b001698281c60ad91356052ca6cf5e9e94a73
Ziemowit Slawinski <ziemowit.slawinskI@gmail.com>
2024-03-29 14:18:30 +0100

    Update run_jupyter.sh

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# Initialize the number of attempts to wait for the log file to be created
-max_attempts=50
-
-# Function to cancel the job and remove the log file
-cancel_job() {
-  echo "Cancelling job $job_id"
-  scancel $job_id
-  if [ -f "$log_file_name" ]; then
-    echo "Removing log file $log_file_name"
-    rm -f $log_file_name
-  fi
-}
-
 # Submit the batch job and capture the output
 output=$(sbatch python-notebook.slurm)
 
@@ -24,32 +11,47 @@ echo "Submitted batch job with ID: $job_id"
 # Construct the log file name using the job ID
 log_file_name="jupyter-log-$job_id.txt"
 
+# Flag to keep track of script running status
+keep_running=true
+
+# Function to cancel the job and remove the log file, then exit script
+cancel_job() {
+  echo "Cancelling job $job_id"
+  scancel $job_id
+  if [ -f "$log_file_name" ]; then
+    echo "Removing log file $log_file_name"
+    rm -f $log_file_name
+  fi
+  keep_running=false
+  exit  # Exit the script immediately
+}
+
 # Set up trap to call cancel_job when the script is interrupted
 trap cancel_job INT TERM
 
-attempt=0
+echo "Waiting for log file to be created..."
 
 # Wait for the log file to be created, checking periodically
-while [ ! -f "$log_file_name" ] && [ $attempt -lt $max_attempts ]; do
-  echo "Waiting for log file to be created... (Attempt: $((attempt+1))/$max_attempts)"
-  sleep 10  # Wait for 10 seconds before checking again
-  ((attempt++))
+while $keep_running; do
+  if [ -f "$log_file_name" ]; then
+    echo "Log file ($log_file_name) was created."
+    echo "Contents of the log file:"
+    cat $log_file_name
+    echo "---------------------------------"
+    break  # Log file found, break out of the loop
+  else
+    sleep 10  # Wait for 10 seconds before checking again
+  fi
 done
 
-# Check if the log file was created within the given number of attempts
-if [ ! -f "$log_file_name" ]; then
-  echo "Error: Log file ($log_file_name) was not created after $max_attempts attempts."
-  exit 1
-else
-  echo "Log file ($log_file_name) was created."
-  echo "Contents of the log file:"
-  cat $log_file_name
-  echo "---------------------------------"
+# Exit the script if cancel_job was called before the log file was created
+if [ "$keep_running" = false ]; then
+  exit
 fi
 
 # After the log file is found, monitor the job status
 echo "Monitoring the job status. Press Ctrl+C to cancel the job and exit."
-while squeue | grep -q $job_id; do
+while $keep_running && squeue | grep -q $job_id; do
   sleep 10
 done
 
